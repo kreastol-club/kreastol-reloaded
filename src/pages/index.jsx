f92151967a7c03a0e5203018263a37ff31e62193
Joshua Hegedus <josh.hegedus@outlook.com>
2023-09-24 23:51:13 +0200

    fixed the tutorial project

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h3>Welcome to the Kreastol main page</h3>
+      <small>The site is under developement</small>
+
+      <p>Check back in a few day!</p>
+    </div>
+  );
+};
+
+export default MainPage;
